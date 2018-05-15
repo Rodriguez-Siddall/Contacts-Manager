61cc14b1d254974d8d8c05cb4b8cbb49c56b82b8
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-05-15 14:13:49 -0500

    added the Contacts class. it includes name and phone number variables and includes getters for them

--- src/contactsManager/Contacts.java
+++ src/contactsManager/Contacts.java
@@ -2,4 +2,22 @@ package contactsManager;
 
 public class Contacts {
 
+
+    String name;
+    long phoneNumber;
+
+    public Contacts(String name, long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
 }
