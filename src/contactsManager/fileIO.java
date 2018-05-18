a794cc2f94ceff73a612aaab23bf103483341457
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-05-18 15:26:33 -0500

    deleted extra lines we weren't using

--- src/contactsManager/fileIO.java
+++ src/contactsManager/fileIO.java
@@ -16,16 +16,12 @@ public class fileIO {
     public static String dir = "contacts";
     public static String filename = "contacts.txt";
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[46m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[47m";
 
 
     // checks to see if a file and dir has been created. if not it makes it
